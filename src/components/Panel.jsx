e756bcbaac557bf8213676da07b53cba00e101dc
Brendan Brown <brendan94brown@gmail.com>
2025-05-21 16:39:02 -0700

    Change and simplify Panel component to use passed onClick handler

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -3,12 +3,12 @@ import React, { Component } from "react";
 
 class Panel extends Component {
   render() {
-    const { id, label, value, onSelect } = this.props;
+    const { label, value, onSelect } = this.props;
 
     return (
       <section
         className="dashboard__panel"
-        onClick={() => onSelect(id)}
+        onClick={onSelect}
       >
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
