3cf67f41cde1f2b9fbd159e9d661584d7adce17b
Brendan Brown <brendan94brown@gmail.com>
2025-05-20 22:48:10 -0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.scss";
+
+import Dashboard from "./components/Dashboard";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>
+);
