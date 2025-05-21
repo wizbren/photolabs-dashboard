3d11aefe3517948cd641957ac6e10e168a30f176
Brendan Brown <brendan94brown@gmail.com>
2025-05-21 14:55:16 -0700

    Add mock data array

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,6 +4,30 @@ import Loading from "./Loading";
 
 
 
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza"
+  }
+];
+
+
 class Dashboard extends Component {
   state = {
     loading: false
