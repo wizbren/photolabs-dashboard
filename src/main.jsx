b536e22b5c2670f92228bb564e2efcec56672d8a
Brendan Brown <brendan94brown@gmail.com>
2025-05-21 09:18:26 -0700

    Add minor formatting changes for preference

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import Dashboard from "./components/Dashboard";
 import "./index.scss";
 
-import Dashboard from "./components/Dashboard";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
