a57a5b9a1c59991aa955305c8df15f76ed2c4648
Brendan Brown <brendan94brown@gmail.com>
2025-05-21 16:17:55 -0700

    Make panels clickable using new click handler function

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -8,6 +8,7 @@ class Panel extends Component {
     return (
       <section
         className="dashboard__panel"
+        onClick={() => onSelect(id)}
       >
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
