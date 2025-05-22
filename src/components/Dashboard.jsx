294aa60c43fa2e9ea87f4f177ce740deae52b8a7
Brendan Brown <brendan94brown@gmail.com>
2025-05-21 21:41:49 -0700

    Add componentDidMount to load focused state from localStorage if it exists; add componentDidUpdate to save any changes made to focused state

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -34,6 +34,19 @@ class Dashboard extends Component {
     focused: null
   };
 
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+    if (focused) {
+      this.setState({focused})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused))
+    }
+  }
+
   selectPanel(id) {
     this.setState(prevState => ({
       focused: prevState.focused
