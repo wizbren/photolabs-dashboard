a835a34a6c79fe8979567a47c355e8adf81b00b9
Brendan Brown <brendan94brown@gmail.com>
2025-05-21 22:07:11 -0700

    Import helper functions from selectors.js, change mock data to use getValue instead of value

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,28 +2,34 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+} from "../helpers/selectors";
 
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalPhotos
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
